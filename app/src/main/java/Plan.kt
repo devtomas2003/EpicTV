d6cf10ccdb8a90397dd8ebb21fc4389284c05b08
Ruben Ferreira <rubenyotb@gmail.com>
2024-12-11 11:49:15 +0000

    Criacao Pagina Planos

---
+++ app/src/main/java/Plan.kt
@@ -0,0 +1,6 @@
+data class Plan(
+    val name: String, // Nome do plano
+    val monthlyPrice: Double, // Preço mensal
+    val annualPrice: Double, // Preço anual
+    val description: String // Descricao
+)
