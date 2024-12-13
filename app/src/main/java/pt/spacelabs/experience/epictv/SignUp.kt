0961ac16f13dbd36435aa2e5919bc6c1cba73988
Madeiras <alexsilva7115@gmail.com>
2024-12-13 03:41:41 -0800

    Continuação da pagina dos planos mensais e anuais <3

--- app/src/main/java/pt/spacelabs/experience/epictv/SignUp.kt
+++ app/src/main/java/pt/spacelabs/experience/epictv/SignUp.kt
@@ -1,13 +1,35 @@
 package pt.spacelabs.experience.epictv
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
 class SignUp : ComponentActivity() {
+
+    private val handler = Handler(Looper.getMainLooper())
+    private val navigateRunnable = Runnable {
+        val intent = Intent(this, Welcome2::class.java)
+        startActivity(intent)
+        overridePendingTransition(R.anim.enter_animation_right, R.anim.exit_animation_right)
+        finish()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.signup)
+
+        handler.postDelayed(navigateRunnable, 5000)
+
+        findViewById<Button>(R.id.button_aderir).setOnClickListener {
+            val intent = Intent (this, Plan::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
 }
